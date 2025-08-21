d24a1f0b3dac1925e0ad253e76a1f7e740de4101
kLaz3r <klazer220033@gmail.com>
2025-08-21 21:42:12 +0300

    done did the Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,28 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="container mx-auto flex min-h-[calc(100vh-5rem)] flex-row items-center justify-between px-6">
-      <div className="flex w-1/2 flex-col gap-2">
-        <h2 className="font-display text-brand-secondary text-2xl font-bold">
+    <section className="container mx-auto flex min-h-[calc(100vh-5rem)] flex-col items-center justify-between gap-12 px-6 md:flex-row">
+      <div className="order-1 flex w-full flex-col gap-2 pt-4 md:order-1 md:w-1/2">
+        <h2 className="font-display text-brand-secondary text-2xl font-bold md:text-2xl">
           RETROWEB
         </h2>
-        <h1 className="font-display text-6xl leading-tight font-black">
+        <h1 className="font-display text-5xl leading-tight font-black md:text-6xl">
           Play Retro Games right in your web browser
         </h1>
-        <p className="font-body text-text-secondary relative w-2/3 text-2xl">
+        <p className="font-body text-text-secondary relative w-full text-xl md:w-2/3 md:text-2xl">
           Turn back the clock with your favorite retro games. Play instantly and
           bring the nostalgia wherever you are.
           <Image
-            className="absolute top-0 right-0 translate-x-35"
+            className="absolute top-0 right-0 translate-y-30 md:translate-x-35 md:translate-y-0"
             src={"/peace.svg"}
-            width={90}
-            height={90}
+            width={60}
+            height={60}
             alt="peace"
           />
         </p>
         <Link
           href="/emulator"
-          className="font-display text-foreground relative mt-3 w-48 px-6 py-3 text-center text-4xl font-bold"
+          className="font-display text-foreground relative mt-3 w-50 px-6 py-3 text-center text-3xl font-bold md:w-48 md:text-4xl"
         >
           <Image
             src={"/arrow-left.svg"}
@@ -40,20 +40,13 @@ const HeroSection = () => {
           </div>
         </Link>
       </div>
-      <div className="flex max-w-1/2 flex-col items-center justify-center">
-        <div className="relative h-[650px] w-[650px]">
+      <div className="order-2 flex max-w-full flex-col items-center justify-center md:order-2 md:max-w-1/2">
+        <div className="relative h-[400px] w-[400px] pt-8 md:h-[650px] md:w-[650px] md:pt-0">
           <Image
             alt="bg"
             src="/images/retro-sun-with-eyes.svg"
             fill
             className="object-contain"
-          />
-          <Image
-            src={"/pacman-with-food.svg"}
-            width={280}
-            height={80}
-            alt="pacman with food"
-            className="absolute bottom-0 translate-x-40 translate-y-20"
           />
         </div>
       </div>
