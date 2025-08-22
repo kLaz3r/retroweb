4d95eb9ee84ea1360d8de5697d34d914fcf3fe92
kLaz3r <klazer220033@gmail.com>
2025-08-22 07:27:35 +0300

    games section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="container mx-auto flex min-h-[calc(100vh-5rem)] flex-col items-center justify-between gap-12 px-6 md:flex-row">
+    <section className="container mx-auto flex min-h-[calc(100vh-5rem)] flex-col items-center justify-between gap-12 overflow-clip px-6 md:flex-row">
       <div className="order-1 flex w-full flex-col gap-2 pt-4 md:order-1 md:w-1/2">
         <motion.h2
           className="font-display text-brand-secondary text-2xl font-bold md:text-2xl"
@@ -133,10 +133,72 @@ const HeroSection = () => {
   );
 };
 
+const games = [
+  {
+    id: 1,
+    img: "/images/zelda.png",
+    title: "The Legend Of Zelda",
+    href: "/emulator",
+  },
+  {
+    id: 2,
+    img: "/images/mario.png",
+    title: "Super Mario World",
+    href: "/emulator",
+  },
+  {
+    id: 3,
+    img: "/images/metroid.png",
+    title: "Metroid Zero Mission",
+    href: "/emulator",
+  },
+  {
+    id: 4,
+    img: "/images/pokemon.png",
+    title: "Pokemon Emerald",
+    href: "/emulator",
+  },
+];
+
+const GamesSection = () => {
+  return (
+    <section className="container mx-auto flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center gap-24 px-6">
+      <div>
+        <h2 className="font-display text-brand-secondary pt-12 text-center text-2xl font-bold md:text-2xl">
+          FAN FAVOURITES
+        </h2>
+        <h1 className="font-display relative pt-4 text-center text-5xl leading-tight font-black md:text-6xl">
+          Start Playing Today
+        </h1>
+      </div>
+      <div className="flex w-full flex-row gap-6">
+        {games.map((game) => {
+          return (
+            <div
+              className="relative flex flex-grow items-center justify-center"
+              key={game.id}
+            >
+              <Image
+                src={game.img}
+                alt={game.title}
+                width={200}
+                height={300}
+                className="z-10 h-full w-full object-cover"
+              />
+              <div className="bg-brand-secondary absolute top-0 left-0 h-full w-full translate-x-2 translate-y-2"></div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
 export default function HomePage() {
   return (
     <main className="bg-background text-foreground min-h-screen">
       <HeroSection />
+      <GamesSection />
     </main>
   );
 }
